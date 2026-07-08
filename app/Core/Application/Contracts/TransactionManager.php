97a9afb24aabb794968840e75a45d1cffc7de752
hieu92264 <nguyenthehieu20182021@gmail.com>
2026-07-08 10:47:18 +0700

    Implement TransactionManager interface and LaravelTransactionManager class for transaction handling

--- app/Core/Application/Contracts/TransactionManager.php
+++ app/Core/Application/Contracts/TransactionManager.php
@@ -1,1 +1,17 @@
 <?php
+
+declare(strict_types=1);
+
+namespace App\Core\Application\Contracts;
+
+interface TransactionManager
+{
+    /**
+     * @template T
+     *
+     * @param Closure(): T $callback
+     *
+     * @return T
+     */
+    public function run(\Closure $callback): mixed;
+}
--- app/Core/Infrastructure/Persistence/LaravelTransactionManager.php
+++ app/Core/Infrastructure/Persistence/LaravelTransactionManager.php
@@ -2,7 +2,22 @@
 
 namespace App\Core\Infrastructure\Persistence;
 
-class LaravelTransactionManager
-{
+use App\Core\Application\Contracts\TransactionManager;
+use Illuminate\Support\Facades\DB;
+use Throwable;
+use Closure;
 
+final class LaravelTransactionManager implements TransactionManager
+{
+    /**
+     * @throws Throwable
+     * => laravel tu dong rollback khi callback nem ra exception hoac tu commit neu call back thanh
+     * cong. Tham so attempts: cho phep thu lai khi gap deadlock
+     */
+    public function run(Closure $callback): mixed
+    {
+        return DB::transaction(
+            $callback, attempts: 3
+        );
+    }
 }
