930f596512a86a4da3b700cefefe94428fee2e34
hieu92264 <nguyenthehieu20182021@gmail.com>
2026-07-08 14:11:30 +0700

    Add validation messages for email, username, and password in English and Vietnamese

--- app/Modules/Identity/Presentation/Resources/lang/en/validation.php
+++ app/Modules/Identity/Presentation/Resources/lang/en/validation.php
@@ -1,1 +1,8 @@
 <?php
+
+return [
+    'email_address_invalid' => 'The provided email address is invalid',
+    'username_invalid' => 'The provided username is invalid. It must be between 3 and 50 characters long.',
+    'username_invalid_characters' => 'Username can only contain lowercase letters, numbers, dots, underscores, and dashes.',
+    'password_empty' => 'Password cannot be empty.',
+];
--- app/Modules/Identity/Presentation/Resources/lang/vi/validation.php
+++ app/Modules/Identity/Presentation/Resources/lang/vi/validation.php
@@ -1,1 +1,8 @@
 <?php
+
+return [
+    'email_address_invalid' => 'Địa chỉ email không hợp lệ.',
+    'username_invalid' => 'Username phải có từ 3 đến 50 ký tự.',
+    'username_invalid_characters' => 'Username chỉ được chứa chữ thường, số, dấu chấm, gạch dưới và gạch ngang.',
+    'password_empty' => 'Mật khẩu không được để trống.'
+];
