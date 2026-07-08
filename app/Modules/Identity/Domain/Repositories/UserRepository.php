917447801339bfb5b99355a004191bbe784919e0
hieu92264 <nguyenthehieu20182021@gmail.com>
2026-07-08 14:12:46 +0700

    Add UserRepository interface with methods for user existence checks and retrieval

--- app/Modules/Identity/Domain/Repositories/UserRepository.php
+++ app/Modules/Identity/Domain/Repositories/UserRepository.php
@@ -1,1 +1,20 @@
 <?php
+
+namespace App\Modules\Identity\Domain\Repositories;
+
+use App\Modules\Identity\Domain\Entities\User;
+use App\Modules\Identity\Domain\ValueObjects\EmailAddress;
+use App\Modules\Identity\Domain\ValueObjects\Username;
+
+interface UserRepository
+{
+    public function existsByEmail(EmailAddress $email): bool;
+
+    public function existsByUsername(Username $username): bool;
+
+    public function findById(int $id): ?User;
+
+    public function findByLogin(string $login): ?User;
+
+    public function save(User $user): User;
+}
