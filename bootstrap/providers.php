fd922afeff284d71f15e6ffe2c6e8259127fc251
hieu92264 <nguyenthehieu20182021@gmail.com>
2026-07-08 11:15:58 +0700

    Register CoreServiceProvider and IdentityServiceProvider in bootstrap providers

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Core\Providers\CoreServiceProvider;
+use App\Modules\Identity\Infrastructure\Providers\IdentityServiceProvider;
 use App\Providers\AppServiceProvider;
 
 return [
     AppServiceProvider::class,
+    CoreServiceProvider::class,
+    IdentityServiceProvider::class
 ];
