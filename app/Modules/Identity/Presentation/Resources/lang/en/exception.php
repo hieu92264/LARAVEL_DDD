cf060575af42007f7fddd689ed6c225ba77dac9c
hieu92264 <nguyenthehieu20182021@gmail.com>
2026-07-08 14:11:39 +0700

    Add exception messages for inactive accounts and existing emails/usernames in English and Vietnamese

--- app/Modules/Identity/Presentation/Resources/lang/en/exception.php
+++ app/Modules/Identity/Presentation/Resources/lang/en/exception.php
@@ -1,1 +1,8 @@
 <?php
+
+return [
+    'account_inactive' => 'Account is not active',
+    'email_already_exists' => 'Email address already exists',
+    'username_already_exists' => 'The username :username is already taken.',
+    'user_not_save' => 'User not saved',
+];
--- app/Modules/Identity/Presentation/Resources/lang/vi/exception.php
+++ app/Modules/Identity/Presentation/Resources/lang/vi/exception.php
@@ -1,1 +1,8 @@
 <?php
+
+return [
+    'account_inactive' => 'Tài khoản đã bị vô hiệu hoá.',
+    'email_already_exists' => 'Địa chỉ email đã tồn tại.',
+    'username_already_exists' => 'Username :username đã được sử dụng.',
+    'user_not_save' => 'User chưa được lưu vào database.',
+];
